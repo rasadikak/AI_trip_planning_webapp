433eb79af0154550820cabea08c3832a0994ec34
kaushani rasadika <rasadikak@gmail.com>
2026-03-05 11:52:19 +0530

    loaded json file

--- backend/features/map/map.py
+++ backend/features/map/map.py
@@ -1,9 +1,13 @@
 from fastapi import APIRouter, FastAPI
-
+import json
 
 router= APIRouter(prefix='/map', tags=['map'])
 
+with open('sri_lanka_places.json','r') as file:
+    places= json.load(file)
+
 @router.get('/')
-def map():
+def map(dest_name:str):
+
     print('map')
     return 'map'
