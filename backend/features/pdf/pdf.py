05af2b43f3d2accfa1f7e1be623811b6cd48effc
kaushani rasadika <rasadikak@gmail.com>
2026-03-09 12:11:34 +0530

    completed pdf downloader

--- backend/features/pdf/pdf.py
+++ backend/features/pdf/pdf.py
@@ -9,41 +9,47 @@ router= APIRouter(prefix='/pdf', tags=['pdf'])
 
 
 def pdf_generate(data:str):
+ 
     pdf= FPDF()
     pdf.set_auto_page_break(auto=True, margin=15)
     pdf.add_page()
 
     font_path = os.path.join(os.path.dirname(__file__), 'fonts', 'DejaVuSans.ttf')
     pdf.add_font('DejaVu', '', font_path, uni=True)
+    font_path2 = os.path.join(os.path.dirname(__file__), 'fonts', 'DejaVuSans-Bold.ttf')
+    pdf.add_font('DejaVu', 'B', font_path2, uni=True)
+
     pdf.set_font('DejaVu', 'B', 18)
     pdf.cell(0, 10, "AI Generated Travel Plan", 0, 1, "C")
     pdf.ln(5)
 
     lines= data.split("\n")
     for line in lines:
-                # headings
+        line = line.strip()  # remove extra spaces
+        if not line:  # skip empty lines
+            continue # headings
         if line.startswith("##"):
             pdf.set_font('DejaVu', "B", 14)
             pdf.ln(5)
-            pdf.cell(0, 10, line.replace("##", ""), ln=True)
+            pdf.cell(pdf.epw, 10, line.replace("##", "").strip(), new_x="LMARGIN", new_y="NEXT")
 
         # sub headings
         elif line.startswith("#"):
             pdf.set_font('DejaVu', "B", 12)
             pdf.ln(3)
-            pdf.cell(0, 8, line.replace("#", ""), ln=True)
+            pdf.cell(pdf.epw, 8, line.replace("#", "").strip(), new_x="LMARGIN", new_y="NEXT")
 
         else:  #normal text
             pdf.set_font('DejaVu', "", 11)
-            pdf.multi_cell(0, 8, line)
+            pdf.multi_cell(pdf.epw, 8, line, new_x="LMARGIN", new_y="NEXT")
     
-    pdf_output= pdf.output(dest="S").encode("utf-8")
-
+    pdf_output = pdf.output() 
     return io.BytesIO(pdf_output)
-
+ 
+   
 #dest="S" → do NOT save to a file, instead return the PDF as a string.
 
-#.encode("latin-1") → converts that string into bytes (binary data).
+#.encode("utf-8") → converts that string into bytes (binary data).
 
 @router.get('/')
 def download_pdf():
