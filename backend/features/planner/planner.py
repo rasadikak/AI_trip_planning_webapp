40cb63c4132c0ae94a8b1495c7fce195b2d3f9fe
kaushani rasadika <rasadikak@gmail.com>
2026-03-10 10:41:21 +0530

    added more food preferences

--- backend/features/planner/planner.py
+++ backend/features/planner/planner.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter,Form
 
 router= APIRouter(prefix='/trip_planner', tags=['trip_planner'])
 
 @router.post('/')
-def trip_planner():
+def trip_planner(destinationType:str= Form(...), budget:str=Form(...), numDays:int=Form(...) ,numPeople:int=Form(...),accommodation:str=Form(...), foodPreference:str=Form(...)):
     pass
