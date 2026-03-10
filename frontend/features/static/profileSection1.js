3d002a9a2dded60530ce5dce952cc869b35cdf36
kaushani rasadika <rasadikak@gmail.com>
2026-03-10 09:50:40 +0530

    installed ollama

--- frontend/features/static/profileSection1.js
+++ frontend/features/static/profileSection1.js
@@ -3,9 +3,10 @@ function get_cookie(name){
     //document.cookie contains all cookies for the current page as a single string.
     //.split(";") turns it into an array of cookies:
      for (let i=0;i<cookie.length;i++){
-        let c=cookie[i].trim();
+        let c=cookie[i].trim(); //remove spaces
         if (c.startsWith(name + '=')){
-            return decodeURIComponent(c.substring(name.length + 1));
+            let value = decodeURIComponent(c.substring(name.length + 1));
+            return value.replace(/^"|"$/g, ''); // removes starting/ending quotes
         }
      }
      //Loop through every cookie in the array.
@@ -22,8 +23,8 @@ document.addEventListener("DOMContentLoaded", function() {
     const name = get_cookie("user_name");
     const email = get_cookie("user_email");
 
-    document.getElementById("name").textContent = name;
-    document.getElementById("email").textContent = email;
+    document.getElementById("name").value = name;
+    document.getElementById("email").value = email;
 });
 
 
