f0e088f34fe5301db86ff3c56be2cf897cee57d1
kaushani rasadika <rasadikak@gmail.com>
2026-03-14 20:24:53 +0530

    updated requiremnets.txt

--- frontend/features/static/chatbotOutput.js
+++ frontend/features/static/chatbotOutput.js
@@ -1,24 +1,40 @@
+console.log("page loaded ");
+
+document.addEventListener("DOMContentLoaded", function(){
+
 document.getElementById("chatForm").addEventListener("submit", async function(e){
     console.log("1 ok");
     e.preventDefault();
     console.log("2 ok");
-    const resultDiv= document.getElementById("chatOutput");
+
+    const resultDiv = document.getElementById("chatOutput");
     console.log("3 ok");
-    resultDiv.innerHTML = "Thinking... Please wait. 🤔"; 
+
+    resultDiv.innerHTML = "Thinking... Please wait. 🤔";
     console.log("4 ok");
+
     try{
-        const formdata= new FormData();
+        const formdata = new FormData();
+        formdata.append("chatInput", document.getElementById("chatInput").value);
+
         const response = await fetch("http://127.0.0.1:8000/chatbot/",{
             method:"POST",
             body: formdata
         });
+
         console.log(response);
-        if (!reponse.ok) throw new Error("Server responded with an error");
-        const result= await response.json();
+
+        if (!response.ok) throw new Error("Server responded with an error");
+
+        const result = await response.json();
         console.log(result);
-        resultDiv.innerHTML= marked.parse(result.response)
+
+        resultDiv.innerHTML = marked.parse(result.response);
+
+    }catch(error){
+        resultDiv.innerHTML = "Error: " + error.message;
     }
-    catch(error){
-        resultDiv.innerHTML= "Error:" + error.message;
-    }
+
+});
+
 });
