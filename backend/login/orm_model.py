4f40229a5a6646e1e31c368295354adad7476fa1
kaushani rasadika <rasadikak@gmail.com>
2026-03-18 22:48:17 +0530

    created chatHistory table

--- backend/login/orm_model.py
+++ backend/login/orm_model.py
@@ -1,8 +1,9 @@
-from sqlalchemy import Column,Integer,String,Boolean
+from sqlalchemy import Column,Integer,String,Boolean, ForeignKey, Text
 import datetime  
 from sqlalchemy.sql.sqltypes import TIMESTAMP
 from sqlalchemy.sql.expression import text
 from backend.login.database import Base
+from sqlalchemy.orm import relationship
 
 class User(Base):
     __tablename__='users'
@@ -12,3 +13,17 @@ class User(Base):
     password= Column(String,nullable=False)
     created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text("now()"))
     is_verified= Column(Boolean,nullable=False, default=False)
+    chat_history = relationship("chatHistory", back_populates="user", cascade="all, delete") 
+
+    
+
+
+class chatHistory(Base):
+    __tablename__='chatHistory'
+    id= Column(Integer, unique=True, index=True, autoincrement=True,nullable=False,primary_key=True)
+    user_id= Column(Integer,nullable=False, ForeignKey("users.id",ondelete="CASCADE"))
+    role=Column(String, nullable=False)
+    content= Column(Text,nullable=False)
+    created_at= Column(TIMESTAMP(timezone=True),nullable=False, server_default=text("now()"))
+    user = relationship("User", back_populates="chat_history")
+    
