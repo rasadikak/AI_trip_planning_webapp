14308708c62c4cd062e5c2f9366f95195211abdf
kaushani rasadika <rasadikak@gmail.com>
2026-03-20 13:45:29 +0530

    updated map

--- backend/features/tripManagement/favDestination.py
+++ backend/features/tripManagement/favDestination.py
@@ -49,7 +49,7 @@ def get_favDestinations(db:Session=Depends(database.get_db),
 
 
 
-@router.delete('/{fav_id}')
+@router.delete('/delete/{fav_id}')
 def delete_fav_destination(
     fav_id: int,
     db: Session = Depends(database.get_db),
