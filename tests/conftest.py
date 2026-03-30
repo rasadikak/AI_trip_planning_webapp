9521b235ae38998b7dbc30aa26e734a1a5820910
kaushani rasadika <rasadikak@gmail.com>
2026-03-30 21:57:04 +0530

    conftest.py done

---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+#Creates a normal test client and a logged-in test client so your tests can call FastAPI endpoints easily
+
+import pytest
+from fastapi.testclient import TestClient
+from backend.main import app  #So now pytest can test your real API routes.
+
+@pytest.fixture(scope="module")
+def client():
+    with TestClient(app) as c:
+        yield c  #This creates a basic test client.
+
+
+@pytest.fixture(scope="module")
+def auth_client():
+    # Returns a client with a logged-in session cookie
+    with TestClient(app) as c: #Creates a test client connected to your FastAPI app.
+       # register a test User
+       c.post("/register/", {
+           "name": "test user",
+           "email":"testUser@gmail.com",
+           "password":"admin123@J",
+           "confirm_password":"admin123@J"
+       })
+
+       #login
+       c.post("/login/",{
+           "username":"testUser@gmail.com",
+           "password":"admin123@J"
+       })
+       yield c
+
+       #So this client is now: Logged-in client
+
+#TestClient-It lets you simulate API requests like: client.get("/home/")
+#without running the real server.
+
+#scope="module" =This means:
+# Create this client once per test file
+# Not every test
