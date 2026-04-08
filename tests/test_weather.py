9a8b48dd215ec5cac4b4c27b428d59dbd65f896c
kaushani rasadika <rasadikak@gmail.com>
2026-04-08 22:14:47 +0530

    test_weather.py

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -8,6 +8,14 @@ def test_weather_success(client):
     })
     print(f" ⭐code for test_weather_success ",response.status_code)
     assert response.status_code==200
+    data= response.json()
+    assert "temp_c" in data
+    assert "feels_like" in data
+    assert "condition" in data
+    assert "humidity" in data
+    assert "wind_kph" in data
+    assert "visibility" in data
+    assert "uv_index" in data
 
 
 
