b61d5913aed4f9b0e61f250c46465fcede203ada
kaushani rasadika <rasadikak@gmail.com>
2026-04-09 11:22:33 +0530

    test_profile.py done

--- tests/test_profile.py
+++ tests/test_profile.py
@@ -1,8 +1,31 @@
 import pytest
-from conftest import client
+
+from tests.conftest import auth_client
 
 
-#no auth mens no cookie
+
+
+
+
+
+
+def test_get_profile_success(auth_client_logged_in):
+    response= auth_client_logged_in.get("/profile/")
+    assert response.status_code==200
+
+
+
+
+def test_editName_auth(auth_client_logged_in):
+    response= auth_client_logged_in.patch("/profile/editName", data={"new_name": "New Name"})
+    assert response.status_code==200
+
+
+
+
+
+#---------------------------------------------
+#no auth means no cookie
 def test_get_profile_unsuccess(client):
     response= client.get("/profile/")
     assert response.status_code==401
