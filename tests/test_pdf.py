9e7c7fbbbb5c5eaac0cfca1742cd81c36290bcf8
kaushani rasadika <rasadikak@gmail.com>
2026-04-09 10:42:39 +0530

    test_pdf done

--- tests/test_pdf.py
+++ tests/test_pdf.py
@@ -1,15 +1,15 @@
 import pytest
-from conftest import client
 
-def test_generate_pdf_success(client):
-    response= client.post("/pdf/", data=
+
+def test_generate_pdf_success(auth_client_logged_in):
+    response= auth_client_logged_in.post("/pdf/", json=
                           {"text":"## Day 1\nVist gampaha"})
     assert response.status_code==200
-    assert response.headers["media_type"]=="application/pdf"
+    assert response.headers["content-type"]=="application/pdf"
 
 
-def test_pdf_empty_text(client):
-    response= client.post("/pdf/", data=
+def test_pdf_empty_text(auth_client_logged_in):
+    response= auth_client_logged_in.post("/pdf/", json=
                           {"text":""})
     assert response.status_code in [200,422]
 
