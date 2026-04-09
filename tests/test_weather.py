1212341772aef5de5a542b9bba6499482b5674f2
kaushani rasadika <rasadikak@gmail.com>
2026-04-09 11:36:58 +0530

    test_weather.py  done

--- tests/test_weather.py
+++ tests/test_weather.py
@@ -1,9 +1,9 @@
-from tests.conftest import client
+
 
 # -------------weather tests-----------
 
-def test_weather_success(client):
-    response= client.post("/weather", data={
+def test_weather_success(auth_client_logged_in):
+    response= auth_client_logged_in.post("/weather/", data={
         "place":"colombo"
     })
     print(f" ⭐code for test_weather_success ",response.status_code)
@@ -19,6 +19,6 @@ def test_weather_success(client):
 
 
 
-def test_weather_empty_place(client):
-    response = client.post("/weather/", data={"place": ""})
+def test_weather_empty_place(auth_client_logged_in):
+    response = auth_client_logged_in.post("/weather/", data={"place": ""})
     assert response.status_code == 422  # FastAPI validation error
