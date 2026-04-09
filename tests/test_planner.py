1d8452d4a670ceae98454d86b85d6ef5f516cfd2
kaushani rasadika <rasadikak@gmail.com>
2026-04-09 10:49:56 +0530

    test_planner done

--- tests/test_planner.py
+++ tests/test_planner.py
@@ -1,26 +1,27 @@
 import pytest
-from conftest import client
 
-def test_planner_success(client):
-    response= client.post("/planner_api/", data=
+
+def test_planner_success(auth_client_logged_in):
+    response= auth_client_logged_in.post("/planner_api/", data=
                           {"destinationType":"city",
                             "budget":"low",
                             "numDays":1 ,
                             "numPeople":1,
                             "accommodation":"hotel",
                             "foodPreference":["spicy"]})
-    assert "output"  in response
+    assert "response"  in response.json()
+    assert response.status_code==200
 
 
-def test_planner_unsuccess_empty_invalid_days(client):
-    response= client.post("/planner_api/", data=
+def test_planner_unsuccess_empty_invalid_days(auth_client_logged_in):
+    response= auth_client_logged_in.post("/planner_api/", data=
                           {})
     assert response.status_code==422
 
 
 
-def test_planner_unsuccess_invalid_days(client):
-    response= client.post("/planner_api/", data=
+def test_planner_unsuccess_invalid_days(auth_client_logged_in):
+    response= auth_client_logged_in.post("/planner_api/", data=
                           {"destinationType":"city",
                             "budget":"low",
                             "numDays":0 ,
